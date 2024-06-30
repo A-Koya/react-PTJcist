912832fc0ca609a51c12992fad68ccd35e150173
A-Koya <a.koya583rec@gmail.com>
2024-06-30 16:58:33 +0900

    mysql wrapper

---
+++ backend/infrastructure/mysql/mysqlHandler.go
@@ -0,0 +1,49 @@
+package mysql
+
+import (
+	"A-Koya/react-PTJcist/adapter/repository"
+	"context"
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type mysqlHandler struct {
+	db *sql.DB
+}
+
+func NewMysqlHandler() (*mysqlHandler, error) {
+	db, err := sql.Open("mysql", "username:password@tcp(localhost:3306)/dbname")
+	if err != nil {
+		fmt.Println("DB connection failed")
+		return &mysqlHandler{}, nil
+	}
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("DB ping failed")
+		return &mysqlHandler{}, nil
+	}
+	defer db.Close()
+	return &mysqlHandler{db: db}, nil
+}
+
+func (m *mysqlHandler) ExecuteContext(ctx context.Context, query string, args ...interface{}) error {
+	_, err := m.db.ExecContext(ctx, query, args...)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func (m *mysqlHandler) QueryContext(ctx context.Context, query string, args ...interface{}) (repository.Rows, error) {
+	rows, err := m.db.QueryContext(ctx, query, args...)
+	if err != nil {
+		return nil, err
+	}
+	row := newMysqlRows(rows)
+	return row, nil
+}
+func (m *mysqlHandler) QueryRowContext(ctx context.Context, query string, args ...interface{}) repository.Row {
+	row := m.db.QueryRowContext(ctx, query, args...)
+	return newMysqlRow(row)
+}
---
+++ backend/infrastructure/mysql/mysqlRowHandler.go
@@ -0,0 +1,47 @@
+package mysql
+
+import (
+	"database/sql"
+)
+
+type mysqlRow struct {
+	row *sql.Row
+}
+
+func newMysqlRow(row *sql.Row) mysqlRow {
+	return mysqlRow{row: row}
+}
+
+func (mr mysqlRow) Scan(dest ...interface{}) error {
+	if err := mr.Scan(dest...); err != nil {
+		return err
+	}
+	return nil
+}
+
+type mysqlRows struct {
+	rows *sql.Rows
+}
+
+func newMysqlRows(rows *sql.Rows) mysqlRows {
+	return mysqlRows{rows: rows}
+}
+
+func (mrs mysqlRows) Scan(dist ...interface{}) error {
+	if err := mrs.rows.Scan(dist...); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (mrs mysqlRows) Next() bool {
+	return mrs.rows.Next()
+}
+
+func (mrs mysqlRows) Err() error {
+	return mrs.rows.Err()
+}
+
+func (mrs mysqlRows) Close() error {
+	return mrs.rows.Close()
+}
